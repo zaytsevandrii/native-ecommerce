bdc2807ab01e7e3bc8e8ee70e42e780252822955
Andrii Zaitsev <105815678+zaytsevandrii@users.noreply.github.com>
2022-12-31 09:20:35 +0000

    add react router and settings

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom"
+import Footer from "./components/Footer/Footer"
+import Navbar from "./components/Navbar/Navbar"
+import Home from "./pages/Home/Home"
+import Product from "./pages/Product/Product"
+import Products from "./pages/Products/Products"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Layout = () => {
+    return (
+        <div className="app">
+            <Navbar />
+            <Outlet />
+            <Footer />
+        </div>
+    )
 }
 
-export default App;
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <Layout />,
+        children: [
+            {
+                path: "/",
+                element: <Home />,
+            },
+            {
+                path: "/products/:id",
+                element: <Products />,
+            },
+            {
+                path: "/product/:id",
+                element: <Product />,
+            },
+        ],
+    },
+])
+function App() {
+    return (
+        <div>
+            <RouterProvider router={router} />
+        </div>
+    )
+}
+
+export default App
