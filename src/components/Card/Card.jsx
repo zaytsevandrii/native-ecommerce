94c62115f3dc072024e3bea585851e4044957f55
Andrii Zaitsev <105815678+zaytsevandrii@users.noreply.github.com>
2023-01-01 14:53:49 +0000

    connect strapi with shop and get res

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -3,18 +3,19 @@ import { Link } from "react-router-dom"
 import "./Card.scss"
 
 const Card = ({ item }) => {
+    console.log(item.attributes.img.data.attributes.url)
     return (
         <Link className="link" to={`/product/${item.id}`}>
             <div className="card">
                 <div className="image">
-                    {item.isNew&&<span>New Season</span>}
-                    <img src={item.img} alt="" className="mainImg" />
-                    <img src={item.img2} alt="" className="secondImg" />
+                    {item?.attributes.isNew&&<span>New Season</span>}
+                    <img src={'http://localhost:1337'+item.attributes.img.data.attributes.url} alt="" className="mainImg" />
+                    <img src={'http://localhost:1337'+item.attributes.img2.data.attributes.url} alt="" className="secondImg" />
                 </div>
-                <h2>{item.title}</h2>
+                <h2>{item?.attributes.title}</h2>
                 <div className="prices">
-                    {item.oldPrice&&<h3>${item.oldPrice}</h3>}
-                    <h3>${item.price}</h3>
+                    <h3>${item.oldPrice||item?.attributes.price+15}</h3>
+                    <h3>${item?.attributes.price}</h3>
                 </div>
             </div>
         </Link>
